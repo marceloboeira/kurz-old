6ad67a114c63ec7d2ea3d98a2dc59bfb14be0148
Marcelo Boeira <me@marceloboeira.com>
2017-07-23 21:27:36 +0200

    feature: Improve performance
     - Do not create instance of the redis url service every time we want to
    handle a request
     - Use a Future to represent the request and let Finagle handle it...

--- src/main/scala/com/kurz/Server.scala
+++ src/main/scala/com/kurz/Server.scala
@@ -2,22 +2,22 @@ package com.kurz
 
 import com.kurz.services.RedisUrlService
 import com.twitter.server.TwitterServer
-import com.twitter.finagle.param.Stats
-import com.twitter.util.{Await, Duration}
+import com.twitter.util.{Await, Duration, Future}
 import com.twitter.finagle.Http
 import com.twitter.finagle.http.Status
 import io.finch._
 
 object Server extends TwitterServer {
   val port = flag("http.port", "5000", "HTTP Server port")
+  val urlService = new RedisUrlService
 
   val redirection: Endpoint[Unit] = get(string) { slug: String =>
-    val urlService = new RedisUrlService
-
-    urlService.find(slug) match {
-      case Some(url) => Output.unit(Status.TemporaryRedirect).withHeader("Location" -> url)
-      case _ => NotFound(new Exception("$slug is not a valid slug"))
-    }
+    Future.value(
+      urlService.find(slug) match {
+        case Some(url) => Output.unit(Status.TemporaryRedirect).withHeader("Location" -> url)
+        case _ => NotFound(new Exception("$slug is not a valid slug"))
+      }
+    )
   }
 
   def main(): Unit = {
