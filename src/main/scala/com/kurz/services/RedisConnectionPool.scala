c5b1168956d226876b51f65167bcca215c266df6
Marcelo Boeira <me@marceloboeira.com>
2017-07-31 18:30:43 +0200

    chore: Use REDIS_URL to connect to redis
    Makes it possible to use Heroku Redis, also, it is better than the
    hardcoded one 🤓

--- src/main/scala/com/kurz/services/RedisConnectionPool.scala
+++ src/main/scala/com/kurz/services/RedisConnectionPool.scala
@@ -8,12 +8,15 @@ object RedisConnectionPool {
   lazy val instance: JedisPool = {
     val poolConfig = new JedisPoolConfig()
 
-    // Move config to ENV
     poolConfig.setMaxTotal(100)
     poolConfig.setMinIdle(100)
     poolConfig.setMaxIdle(100)
     poolConfig.setMaxWaitMillis(10000)
 
-    new JedisPool(poolConfig, new URI("redis://127.0.0.1:6379"), 10000)
+    new JedisPool(poolConfig, connectionURI, 10000)
+  }
+
+  lazy val connectionURI: URI = {
+    new URI(Option(System.getenv("REDIS_URL")).getOrElse("redis://127.0.0.1:6379"))
   }
 }
